bd9621dd132bcfa14be0a677db3f15b6ca63c964
hackerbhind <144337511+hackerbhind@users.noreply.github.com>
2023-10-31 15:53:20 +0530

    Nuke wfdcommon

--- device.mk
+++ device.mk
@@ -488,8 +488,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#WfdCommon
 
 # Inherit the proprietary files
 include vendor/oneplus/oscaro/oscaro-vendor.mk
